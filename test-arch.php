22fd3ac03586bcbbd2a564516fde7657ee2c4944
plugh3 <plugh03@gmail.com>
2016-03-01 09:42:40 -0800

    test directories

--- test-arch.php
+++ test-arch.php
@@ -4,3 +4,20 @@
 //$item_fname2iname = array('k1' => 'v1', 'k2' => 'v2'); // hash
 
 echo ">>> php_uname() ".php_uname("s")."\n";
+
+switch (strtolower(php_uname("s"))) {
+    case "darwin":
+        $dir_home = getenv("HOME");
+        $dir_export = $dir_home.'/Library/Application Support/EVE Online/p_drive/User/My Documents/EVE/logs/Marketlogs';
+        break;
+    case "windows nt":
+        $dir_home = getenv("HOMEDRIVE").getenv("HOMEPATH");
+        $dir_export = $dir_home.'\\Documents\\EVE\\logs\\Marketlogs';
+        break;
+    default:
+        echo ">>> unknown OS type ".php_uname("s")."\n";
+        exit;
+}        
+echo ">>> home ".$dir_home."\n";
+echo ">>> logs ".$dir_export."\n";
+if (file_exists($dir_export)) { echo "Success!\n"; } else { echo "Failure\n"; }
