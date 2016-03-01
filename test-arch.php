e1d4c65a3a19c6bf5151928423dfa8f3e6ef98bb
plugh3 <plugh03@gmail.com>
2016-03-01 09:31:14 -0800

    test php environment variables

---
+++ test-arch.php
@@ -0,0 +1,6 @@
+<?php
+
+//echo time2s()."defer ".($last2[$row] + 5*60 - $time)."s $reg_name-$item_name\n"; // print
+//$item_fname2iname = array('k1' => 'v1', 'k2' => 'v2'); // hash
+
+echo ">>> php_uname() ".php_uname("s")."\n";
