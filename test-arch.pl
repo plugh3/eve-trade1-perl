1bc57d0325627a735dd01f5b01aa5cee373cefaf
plugh3 <plugh03@gmail.com>
2016-03-03 18:05:28 -0800

    test - fix filepath parsing for multi-OS

--- test-arch.pl
+++ test-arch.pl
@@ -23,8 +23,30 @@ while (($k, $v) = each %ENV) {
 	print ">>> item >".$item."<\n";
 =cut
 
-	my $dirname = "/Users/csserra/Library/Application Support/EVE Online/p_drive/User/My Documents/EVE/logs/Marketlogs/";
+	my $dirname = 'C:\Users\csserra\Documents\EVE\logs\Marketlogs';
 	my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$modtime,$ctime,$blksize,$blocks) = stat($dirname);
 	print ">>> directory last modified $modtime >$dirname<\n";
 	my $DIR;
 	opendir($DIR, $dirname) or die "directory.open failed: >$dirname<";
+
+	### TODO: not working on win32 !!!
+	my $fname = 'C:\Users\csserra\Documents\EVE\logs\Marketlogs\Domain-150mm Light \'Scout\' Autocannon I-2016.03.01 222448.txt';
+	print ">>> fname >$fname<\n";
+	print ">>> dirname >$dirname<\n";
+	my $dir_sep = "\\";
+	my $path_esc = $dirname.$dir_sep;
+	$path_esc =~ s/\\/\\\\/g;
+	print ">>> esc >$dir_marketlogs_esc<\n";
+	$fname =~ /^$path_esc(?<region>[^-]+?)-(?<item>.*)-(?<yr>[0-9]{4})\.(?<mo>[0-9][0-9])\.(?<dy>[0-9][0-9]) (?<hh>[0-9][0-9])(?<mm>[0-9][0-9])(?<ss>[0-9][0-9])\.txt$/;
+	my $fileRegName = $+{region};
+	print ">>> fileRegName >$fileRegName<\n";
+
+my $dir_sep = "\\";
+sub strip_dir {
+	my ($x) = @_;
+	my @tokens = split(quotemeta($dir_sep), $x);
+	return $tokens[@tokens-1];
+}
+print ">>> unstripped ".$fname."\n";
+print ">>> stripped ".&strip_dir($fname)."\n";
+
